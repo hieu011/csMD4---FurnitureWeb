4071748a9a92c6ef97965bf0e004c582a0243d73
DSPHUC <phuc.dongsi@gmail.com>
2023-11-22 16:43:40 +0700

    fix logic check login

--- src/main/java/com/example/furnitureweb/service/auth/AuthService.java
+++ src/main/java/com/example/furnitureweb/service/auth/AuthService.java
@@ -65,8 +65,10 @@ public class AuthService implements UserDetailsService {
         boolean check = false;
         Optional<User> userOptional = userRepository.findByUsername(request.getUsername());
         if (userOptional.isEmpty()
-                || !passwordEncoder.encode(userOptional.get().getPassword())
-                                            .equals(request.getPassword())) {
+                || passwordEncoder.matches(userOptional.get().getPassword(),request.getPassword())) {
+            System.out.println(userOptional.get().getPassword());
+            System.out.println((request.getPassword()));
+            System.out.println(passwordEncoder.encode(request.getPassword()));
             result.rejectValue("password", "password",
                     "Tài khoản hoặc mật khẩu không đúng");
             check = true;
